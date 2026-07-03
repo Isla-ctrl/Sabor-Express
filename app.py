0040c90ecbffad38d7e0fa7aab9066f5522791c2
Isla-ctrl <bzsouza1303@gmail.com>
2026-07-03 15:10:34 -0300

    Adiciona metodo de recebimento de valor com mesmo tipo desejado

--- app.py
+++ app.py
@@ -13,11 +13,14 @@ print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}!')
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+# print(opcao_escolhida == 1)
+# print(type(opcao_escolhida))
+# print(type(1))  #metodo de ver o tipo do valor
 
 if opcao_escolhida == 1:
-    print('cadastrar restaurante')
+    print('Cadastrar restaurante')
 elif opcao_escolhida == 2:
     print('Listar restaurante')
 elif opcao_escolhida == 3:
